2d5acf90049659f4390f0cbc129279078e3e054d
Aaryan <aiml.student2807@gmail.com>
2026-03-24 23:53:45 +0530

    Remove user package.json file from the frontend directory

--- Frontend/Frontend/forgotpassword.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('forgotPasswordForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var email = document.getElementById('email').value;
-    // Simulate sending a password reset email
-    alert('Password reset link has been sent to ' + email);
-});
